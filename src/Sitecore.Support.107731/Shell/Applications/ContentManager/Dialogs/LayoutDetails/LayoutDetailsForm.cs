c74a4b0c1c234f15dfae184354aa1ac7cb94ed3d
MichaelToh <ttk@sitecore.net>
2017-12-04 18:01:15 +0800

    First release for 8.2.3

---
+++ src/Sitecore.Support.107731/Shell/Applications/ContentManager/Dialogs/LayoutDetails/LayoutDetailsForm.cs
@@ -0,0 +1,43 @@
+﻿namespace Sitecore.Support.Shell.Applications.ContentManager.Dialogs.LayoutDetails
+{
+    using Sitecore.Data.Fields;
+    using Sitecore.Diagnostics;
+    using Sitecore.Xml.Patch;
+
+    /// <summary>
+    /// Represents a gallery layout form.
+    /// </summary>
+    public class LayoutDetailsForm : Sitecore.Shell.Applications.ContentManager.Dialogs.LayoutDetails.LayoutDetailsForm
+    {
+        /// <summary>
+        /// Gets or sets the final layout.
+        /// </summary>
+        /// <value>The final layout.</value>
+        public override string FinalLayout
+        {
+            get
+            {
+                string layoutDelta = this.LayoutDelta;
+                if (string.IsNullOrWhiteSpace(layoutDelta))
+                {
+                    return this.Layout;
+                }
+                if (XmlPatchUtils.IsXmlPatch(layoutDelta))
+                {
+                    return XmlDeltas.ApplyDelta(this.Layout, layoutDelta);
+                }
+                return layoutDelta;
+            }
+            set
+            {
+                Assert.ArgumentNotNull(value, "value");
+                if (!string.IsNullOrWhiteSpace(this.Layout))
+                {
+                    this.LayoutDelta = XmlDeltas.GetDelta(value, this.Layout);
+                    return;
+                }
+                this.LayoutDelta = value;
+            }
+        }
+    }
+}
